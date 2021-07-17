9ee97579cfc131cf15a92a91a98105f95b29cb04
alarangoor <alarangoor@unal.edu.co>
2021-07-17 18:25:56 -0500

    Se creó la actividad de términos y condiciones

---
+++ app/src/main/java/com/mintic/myapplication/TermsActivity.java
@@ -0,0 +1,28 @@
+package com.mintic.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class TermsActivity extends AppCompatActivity {
+
+    private Button btn_back;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_terms);
+
+        btn_back = findViewById(R.id.btn_back);
+
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
