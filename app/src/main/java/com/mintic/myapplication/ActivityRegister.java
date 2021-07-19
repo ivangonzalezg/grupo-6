725664741a43a2ad813fde1eb35921f4d1cce344
Ivan Gonzalez <ivangonzalezgrc@gmail.com>
2021-07-18 20:49:20 -0500

    Activity register

--- app/src/main/java/com/mintic/myapplication/ActivityRegister.java
+++ app/src/main/java/com/mintic/myapplication/ActivityRegister.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -14,6 +15,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class ActivityRegister extends AppCompatActivity {
+    private static final String TAG = "ActivityRegister";
 
     private EditText tx_names;
     private EditText tx_lnames;
@@ -27,14 +29,14 @@ public class ActivityRegister extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
 
         tx_names = findViewById(R.id.tx_names);
         tx_lnames = findViewById(R.id.tx_lname);
-        tx_mail =   findViewById(R.id.tx_email);
+        tx_mail = findViewById(R.id.tx_email);
         tx_pwd = findViewById(R.id.tx_pwd);
         chk_terminos = findViewById(R.id.chk_terminos);
-        act_terminos = findViewById(R.id.Act_Terminos);
+        act_terminos = findViewById(R.id.act_terminos);
         btn_enviar = findViewById((R.id.btn_enviar));
         mySelf = this;
 
@@ -46,8 +48,8 @@ public class ActivityRegister extends AppCompatActivity {
                 /*Intent terminos = new Intent(mySelf, TermCond.class);
                 startActivity((terminos));
 */
-                chk_terminos.setEnabled(true);
 
+                Log.i(TAG, "onClick: abrir terminos");
 
             }
         });
@@ -55,10 +57,9 @@ public class ActivityRegister extends AppCompatActivity {
         chk_terminos.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked){
+                if (isChecked) {
                     btn_enviar.setEnabled(true);
-                }
-                else{
+                } else {
                     btn_enviar.setEnabled(false);
                 }
             }
@@ -80,6 +81,8 @@ public class ActivityRegister extends AppCompatActivity {
 
                 ActivityLogin.putExtra("username", username);
                 ActivityLogin.putExtra("password", password);*/
+
+                Log.i(TAG, "onClick: " + username + " " + password);
 
             }
         });
