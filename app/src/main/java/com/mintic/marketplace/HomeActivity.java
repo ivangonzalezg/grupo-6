fda32b07e28b4af65147966d2a973e95cf49908f
Ivan Gonzalez <ivangonzalezgrc@gmail.com>
2021-08-02 13:47:52 -0500

    Menú en home activity, opción de cerrar sesión lista

--- app/src/main/java/com/mintic/marketplace/HomeActivity.java
+++ app/src/main/java/com/mintic/marketplace/HomeActivity.java
@@ -1,14 +1,41 @@
 package com.mintic.marketplace;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.mintic.marketplace.utils.SharedPref;
 
 public class HomeActivity extends AppCompatActivity {
+    private static final String TAG = "HomeActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.log_out_item) {
+            FirebaseAuth auth = FirebaseAuth.getInstance();
+            auth.signOut();
+            SharedPref.clear(HomeActivity.this);
+            Intent intent = new Intent(HomeActivity.this, LoginActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
