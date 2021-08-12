bc9460ba6a11c3e8e6a7de233226b4d9ed4a41e9
Ivan Gonzalez <ivangonzalezgrc@gmail.com>
2021-08-12 17:59:48 -0500

    Añadiendo soporte para modo oscuro

--- app/src/main/java/com/mintic/marketplace/TermsAndConditionsActivity.java
+++ app/src/main/java/com/mintic/marketplace/TermsAndConditionsActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.Objects;
+
 public class TermsAndConditionsActivity extends AppCompatActivity {
     private static final String TAG = "TermsAndConditionsActiv";
 
@@ -11,5 +13,7 @@ public class TermsAndConditionsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_terms_and_conditions);
+
+        Objects.requireNonNull(getSupportActionBar()).setTitle(R.string.terms_and_conditions_title);
     }
 }
